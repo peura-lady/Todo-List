d5000be184ce5a86673751edc439c756fec93a5c
peura-lady <anna.sonninen94@gmail.com>
2021-09-22 14:22:12 +0300

    added grid function

---
+++ src/components/TodoTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function TodoTable(props) {
+    return (
+        <div>
+                       <table>
+                <tbody>
+                    <tr>
+                        <th>Descr</th>
+                        <th>Date</th>
+                    </tr>
+                    {
+                        props.todos.map((todo, index) =>
+                            <tr key={index}>
+                                <td>{todo.desc}</td>
+                                <td>{todo.date}</td>
+                                <td>
+                                    <button onClick={() => props.deleteTodo(index)}>Delete</button>
+                                </td>
+                            </tr>)
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default TodoTable
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,38 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
 
 function Todos() {
-    const [desc, setDesc] = useState('');
-    const [date, setDate] = useState('');
-    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = React.useState({ desc: '', date: '', priority: '' });
+    const [todos, setTodos] = React.useState([]);
 
     const addTodo = () => {
-        setTodos([...todos, desc]);
+        setTodos([todo, ...todos]);
     }
+
+    const inputChanged = (event) => {
+        setTodo({ ...todo, [event.target.name]: event.target.value });
+    }
+
+    const deleteTodo = (row) => {
+       setTodos(todos.filter((todo, index) => index !== row));
+    }
+
+    const columns = [
+        {field: 'desc', sortable: true, filter: true},
+        {field: 'date', sortable: true, filter: true},
+        {field: 'priority', sortable: true, filter: true, 
+        cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}}
+    ]
+
 
     return (
         <div>
-            Description: <input value={desc} onChange={e => setDesc(e.target.value)} />
-            Date: <input value={date} onChange={e => setDate(e.target.value)} />
-            <button onClick={addTodo}> Add </button>
-            <table>
-            <br />
-                <tbody>
-                    <tr>
-                        <th>Description</th>
-                        <th>Date</th>
-                    </tr>
-                    {
-                        todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo}</td>
-                                <td>{date}</td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+            <h2>Todo list</h2>
+            <input placeholder = "Description" name="desc" value={todo.desc} onChange={inputChanged} />
+            <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+            <input placeholder = "Priority" name="priority" value={todo.priority} onChange={inputChanged} />
+
+
+            <button onClick={addTodo}>Add</button>
+            <div className="ag-theme-material" style={{height: 400, width: 600, margin: 'auto'}}>
+            <AgGridReact 
+                rowData={todos}
+                columnDefs={columns}
+            />
+            </div>
         </div>
     );
 }
+
 
 export default Todos;
